beac02c375099cef12d9d484dc25f714448a66e5
claudio <criquelmemoreno@gmail.com>
2020-04-13 11:26:49 -0400

    change query

--- application/models/Model.php
+++ application/models/Model.php
@@ -5,20 +5,9 @@ class Model extends CI_Model{
         $db['default']['db_debug'] = FALSE;
 
         $query="SELECT SQL_CALC_FOUND_ROWS p.`id_product`  AS `id_product`,
-        p.`reference`  AS `reference`,
-        sa.`price`  AS `price`,
-        p.`id_shop_default`  AS `id_shop_default`,
-        p.`is_virtual`  AS `is_virtual`,
         pl.`name`  AS `name`,
-        pl.`link_rewrite`  AS `link_rewrite`,
-        sa.`active`  AS `active`,
-        shop.`name`  AS `shopname`,
         image_shop.`id_image`  AS `id_image`,
-        cl.`name`  AS `name_category`,
-        0 AS `price_final`,
-        pd.`nb_downloadable`  AS `nb_downloadable`,
-        sav.`quantity`  AS `sav_quantity`,
-        IF(sav.`quantity`<=0, 1, 0) AS `badge_danger` 
+        cl.`name`  AS `name_category`
        FROM  `ps6d_product` p 
         LEFT JOIN `ps6d_product_lang` pl ON (pl.`id_product` = p.`id_product` AND pl.`id_lang` = 2 AND pl.`id_shop` = 1) 
         LEFT JOIN `ps6d_stock_available` sav ON (sav.`id_product` = p.`id_product` AND sav.`id_product_attribute` = 0 AND sav.id_shop = 1  AND sav.id_shop_group = 0 ) 
@@ -31,10 +20,7 @@ class Model extends CI_Model{
         LEFT JOIN `ps6d_product_download` pd ON (pd.`id_product` = p.`id_product`) 
        WHERE (1 AND state = 1)
         
-       ORDER BY  `id_product` desc
-        
-       LIMIT 0, 20
-       ;";
+       ORDER BY  `id_product` desc";
         $resultados = $this->db->query($query);
         return $resultados->result();
     }
